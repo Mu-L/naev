be7f596c87cac551a050d4e047b87e02bcc83b22
Edgar Simo-Serra <bobbens@gmail.com>
2026-02-12 14:54:56 +0900

    Start to add yearning steve.

--- dat/scripts/common/trader.lua
+++ dat/scripts/common/trader.lua
@@ -3,8 +3,25 @@
    Trader Common Functions
 
 --]]
+local vn = require "vn"
+
 local trader = {}
 
 trader.prefix = "#b".._("TRADER: ").."#0"
+
+trader.vrata_steve = {
+   portrait = "vrata/unique/yearning_steve",
+   image = "vrata/unique/yearning_steve",
+   name = _("Steve"),
+   colour = nil,
+   description = _("TODO"),
+}
+function trader.vn_vrata_steve( params )
+   return vn.Character.new( trader.vrata_steve.name,
+         tmerge( {
+            image=trader.vrata_steve.image,
+            colour=trader.vrata_steve.colour,
+         }, params) )
+end
 
 return trader
