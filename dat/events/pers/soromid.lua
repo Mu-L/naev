9ac3b788d1cdd0003e3976907d3fa4de20ad9fd4
Edgar Simo-Serra <bobbens@gmail.com>
2026-02-12 11:22:00 +0900

    Added some more soromid pers.

--- dat/events/pers/soromid.lua
+++ dat/events/pers/soromid.lua
@@ -28,8 +28,7 @@ return function ()
                m.bribe_no = _([["We do not deal with the tainted."]])
                return p
             end,
-         },
-         {
+         }, {
             spawn = function ()
                -- Irala = maori unisex name that means god wrestler
                local p = pilot.add("Soromid Odium", "Soromid", nil, _("Witch Doctor Irala"), {naked=true, ai="pers"})
@@ -46,7 +45,49 @@ return function ()
                m.bribe_no = _([["We do not deal with the tainted."]])
                return p
             end,
-         },
+         }, {
+            spawn = function ()
+               local p = pilot.add("Soromid Nyx", "Soromid", nil, _("Warrior Renata"), {naked=true, ai="pers_patrol"})
+               p:outfitAddIntrinsic("Escape Pod")
+               equipopt.soromid( p, { bioship_stage=9,
+                     bioship_skills={
+                        "bite1", "bite2", "bite3", "bite4",
+                        "plasma1", "plasma2", "plasma3",
+                        "health1","health2"},
+                     outfits_add={"Plasma Eruptor"},
+                     prefer={
+                        ["Plasma Eruptor"] = 100,
+                     },
+                  } )
+               local m = p:memory()
+               m.capturable = true
+               m.comm_greet = _([["These are troubled times. Keep safe wanderer."]])
+               m.taunt = _("Finally, a challenge!")
+               m.bribe_no = _([["Do not patronize me with your grubby credits."]])
+               return p
+            end,
+         }, {
+            spawn = function ()
+               local p = pilot.add("Soromid Odium", "Soromid", nil, _("Mosi the Spry"), {naked=true, ai="pers_patrol"})
+               p:outfitAddIntrinsic("Escape Pod")
+               equipopt.soromid( p, { bioship_stage=8,
+                     bioship_skills={
+                        "bite1", "bite2", "bite3",
+                        "move1", "move2", "move3", "move4",
+                        "stealth1",},
+                     outfits_add={"Orion Lance"},
+                     prefer={
+                        ["Orion Lance"] = 100,
+                     },
+                  } )
+               local m = p:memory()
+               m.capturable = true
+               m.comm_greet = _([["I've always to try to eat some jellywhale.")]])
+               m.taunt = _("Your head shall make a good trophy.")
+               m.bribe_no = _([["Credits will not get me a fluffier tail!"]])
+               return p
+            end,
+         }
       } do
          table.insert( pers, v )
       end
