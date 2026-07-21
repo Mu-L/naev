97f4b0b4914ef57884b34b57e0bf7a0cb6826d33
Edgar Simo-Serra <bobbens@gmail.com>
2026-07-22 08:46:49 +0900

    Correct squadron synchronizer module description. Gives 5 FP.

--- dat/outfits/unique/squadron_synchronizer_module.lua
+++ dat/outfits/unique/squadron_synchronizer_module.lua
@@ -2,5 +2,5 @@ require "outfits.unique.poi_data"
 local fmt = require "format"
 
 function descextra( _p, _o )
-   return fmt.f(_("Increases Fleet Capacity by {amt}. Does not need to be equipped."), {amt=25})
+   return fmt.f(_("Increases Fleet Capacity by {amt}. Does not need to be equipped."), {amt=5})
 end
