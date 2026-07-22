1d014c016e194ed85b8df062864d32cf308b28e5
Edgar Simo-Serra <bobbens@gmail.com>
2026-07-23 08:14:01 +0900

    Give a message indicating that flow resonators / meditation chambers only work with normal flow stuff.

--- dat/outfits/flow/structure/flow_resonator.lua
+++ dat/outfits/flow/structure/flow_resonator.lua
@@ -3,7 +3,11 @@ notactive = true -- Doesn't become active
 local flow = require "ships.lua.lib.flow"
 local fmt = require "format"
 
-function descextra( _p, o )
-   return fmt.f("#y".._("Provides {regen} flow regeneration per second up to 50% maximum capacity. Requires a flow amplifier.").."#0",
+function descextra( p, o )
+   local desc = fmt.f("#y".._("Provides {regen} flow regeneration per second up to 50% maximum capacity. Requires a flow amplifier.").."#0",
       { regen=flow.list_regen[o:nameRaw()] })
+   if p and flow.is_dreamer( p ) and not o:tags().dreamer then
+      desc = desc.."\n\n#r".._("Requires a flow amplifier to use.").."#0"
+   end
+   return desc
 end
